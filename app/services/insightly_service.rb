a3a18f3eb8799611305b92cd49777055002a5cfe
Kevin Finley <kftwotwo@gmail.com>
2017-01-28 15:44:13 -0800

    136602003: add CRUD methods for Insightly contacts API

--- app/services/insightly_service.rb
+++ app/services/insightly_service.rb
@@ -1,3 +1,25 @@
 class InsightlyService
   Insightly2.api_key = ENV['INSIGHTLY_API_KEY']
+
+  class << self
+    def get_contacts
+      Insightly2.client.get_contacts
+    end
+
+    def get_contact(contact_id)
+      Insightly2.client.get_contact(id: contact_id)
+    end
+
+    def delete_contact(contact_id)
+      Insightly2.client.delete_contact(id: contact_id)
+    end
+
+    def create_contact(contact_attributes)
+      Insightly2.client.create_contact(contact: contact_attributes)
+    end
+
+    def update_contact(contact_attributes)
+      Insightly2.client.update_contact(contact: contact_attributes)
+    end
+  end
 end
